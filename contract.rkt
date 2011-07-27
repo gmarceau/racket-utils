6e2dde8e34707ec910af3bc7bf28b5df64c4f19b
gmarceau <gmarceau@44c62b72-3834-11df-ad18-0b8dd49783f6>
2011-07-27 02:21:18 +0000

    git-svn-id: svn+ssh://cs.wpi.edu/csusers/gmarceau/svnroot/gmarceau-collects@20 44c62b72-3834-11df-ad18-0b8dd49783f6

---
+++ contract.rkt
@@ -0,0 +1,23 @@
+#lang racket
+(provide (all-defined-out))
+
+(require unstable/contract unstable/function)
+
+(provide list-even-length/c)
+(define list-even-length/c (rename-contract (and/c list? (lambda (lst) (even? (length lst))))
+                                            'list-even-length/c))
+
+(define (map-pairwise k-fn v-fn lst)
+  (let loop ([even #f] [lst lst])
+    (cond [(empty? lst) empty]
+          [(not even) (cons (k-fn (first lst)) (loop (not even) (rest lst)))]
+          [else (cons (v-fn (first lst)) (loop (not even) (rest lst)))])))
+
+(define (list-pairwise/c k/c v/c)
+  (define name (build-compound-type-name 'list-pairwise/c k/c v/c))
+  (rename-contract
+   (and/c list-even-length/c
+          (make-contract #:name name
+                         #:first-order (lambda (lst) (andmap identity (map-pairwise (contract-first-order k/c) (contract-first-order v/c) lst)))
+                         #:projection (lambda (blame) (lambda (lst) (map-pairwise ((contract-projection k/c) blame) ((contract-projection v/c) blame) lst)))))
+   name))
